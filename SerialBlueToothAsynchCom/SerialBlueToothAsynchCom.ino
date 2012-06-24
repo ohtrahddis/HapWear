a429c83c3400aff36e561a0a1510c009d56349ee
Siddhartho Bhattacharya <sid_gogol@hotmail.com>
2012-06-24 14:03:03 -0700

    updates to bluetooth test

--- SerialBlueToothAsynchCom/SerialBlueToothAsynchCom.ino
+++ SerialBlueToothAsynchCom/SerialBlueToothAsynchCom.ino
@@ -1,11 +1,11 @@
-#include <ctype.h>
+//#include <ctype.h>
 #include <SoftwareSerial.h>
-
+/*
 #define bit9600Delay 84  
 #define halfBit9600Delay 42
 #define bit4800Delay 188 
 #define halfBit4800Delay 94 
-
+*/
 byte rx = 6;
 byte tx = 7;
 byte SWval;
@@ -13,20 +13,20 @@ byte SWval;
 char sbuff[32];
 const char* sendbuff = "hello hai";
 
-SoftwareSerial mySerial(rx, tx);
+//SoftwareSerial mySerial(rx, tx);
 
 void setup(){
   Serial.begin(9600);
-  mySerial.begin(9600);
+  //mySerial.begin(9600);
 }
 
 void loop(){
   int sbuffpos = 0;
-  int numchars = mySerial.available();
+  int numchars = Serial.available();
   if(numchars > 31){
     numchars = 0;
   }
-  for(sbuffpos = 0; sbuffpos<numchars; sbuffpos++){
+  /*for(sbuffpos = 0; sbuffpos<numchars; sbuffpos++){
     sbuff[sbuffpos] = (char)mySerial.read();
   }
   sbuff[sbuffpos] = 0;
@@ -40,7 +40,8 @@ void loop(){
     //{
     //  Serial.write("got hello");
     //}
-  }
-  mySerial.write("hi\0");
+  }*/
+  //mySerial.write("hi\0");
+  Serial.readBytes(sbuff, 8);
   delay(1000);
 }
